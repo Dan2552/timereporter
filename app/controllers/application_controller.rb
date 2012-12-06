3d95edd57fc80ad33702f0d6096b0082e3aa2a63
BradleyJennings <bradley.jennings@alliants.co.uk>
2012-12-06 19:07:42 +0000

    split layout into two, devise and application
    when user is not logged in, app will use the devise layout and vice
    versa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :set_layout
+
+  private
+
+  def set_layout
+    user_signed_in? ? "application" : "devise"
+  end
 end
