9dfd3e99bb9541a83695fe8e42624d66b01000b2
BradleyJennings <bradley.jennings@alliants.co.uk>
2012-12-10 08:48:17 +0000

    updated time entries controller methods

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,6 +2,59 @@ class TimeEntriesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @time_entries = User.find(current_user).time_entries
+
+    @date = Date.today
+    @start_of_week = @date.beginning_of_week
+    @end_of_week = @date.end_of_week - 2
+
+    respond_to do |format|
+      format.html
+    end
   end
 
+  def show
+    @time_entry = TimeEntry.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @time_entry = TimeEntry.new(params)
+    @time_entry.user = current_user
+
+    respond_to do |format|
+      if @time_entry.save
+        format.js
+      else
+        format.html
+      end
+    end
+  end
+
+
+  def update
+    @time_entry = TimeEntry.find(params[:id])
+    respond_to do |format|
+      if @time_entry.update_attributes(params)
+        format.js
+      else
+        format.html
+      end
+    end
+  end
+
+
+  def destroy
+    @time_entry = TimeEntry.find(params[:id])
+    @time_entry.destroy
+
+    respond_to do |format|
+      format.html { redirect_to time_entries_url }
+    end
+  end
+
+
 end
+
