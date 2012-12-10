242f39951fbf4a8ee6776fba6dccb0b31a04ffb1
BradleyJennings <bradley.jennings@alliants.co.uk>
2012-12-10 08:49:25 +0000

    updated models

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,5 @@
 class TimeEntry < ActiveRecord::Base
+  attr_accessible :entry_datetime, :duration, :user_id
 
   belongs_to :project
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  has_many :time_entries
 end
