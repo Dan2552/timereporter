4ec70d969e17ae2395c5ea96946e4f06483530f0
Daniel Green <daniel.green@alliants.co.uk>
2013-01-07 13:16:01 +0000

    (experimental) API session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121204211633) do
+ActiveRecord::Schema.define(:version => 20121223194409) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20121204211633) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "api_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
