ad0761770eb3284c09988193404a5a7940fda894
Daniel Green <daniel.green@alliants.co.uk>
2013-01-11 20:40:29 +0000

    fix for pages not rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout :set_layout
 
-  respond_to :json
+  respond_to :json, :html, :js
   def verified_request?
     if request.content_type == "application/json"
       if params[:api_token].present? && params[:api_token] == current_user.try(:api_token)
