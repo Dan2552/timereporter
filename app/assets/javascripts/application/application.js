a0f7cb5e979330a94231434790a39b4f60423658
BradleyJennings <bradley.jennings@alliants.co.uk>
2013-01-16 08:45:40 +0000

    require chosen in js manifest

--- app/assets/javascripts/application/application.js
+++ app/assets/javascripts/application/application.js
@@ -2,6 +2,8 @@
 //= require jquery-ui
 //= require jquery_ujs
 
+//= require chosen.jquery.min
+
 //= require application/flash_message
 //= require application/time_entries
 //= require application/projects
