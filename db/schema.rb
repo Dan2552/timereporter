8a3ea77a4b5875908d5449bb27b30a5d82db8862
Daniel Green <daniel.green@alliants.co.uk>
2013-01-20 01:18:10 +0000

    Add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115235003) do
+ActiveRecord::Schema.define(:version => 20130120011617) do
 
   create_table "clients", :force => true do |t|
     t.datetime "created_at"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20130115235003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
