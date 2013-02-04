a401a46b2def6e084a6b23fd6ac722639cb0fd80
Daniel Green <daniel.green@alliants.co.uk>
2013-02-04 19:26:24 +0000

    Make it easier to debug podio

--- app/models/podio_project_fetcher.rb
+++ app/models/podio_project_fetcher.rb
@@ -3,6 +3,11 @@ class PodioProjectFetcher
   SECRET = 'FO5dAQAKqZEEKh04HDEguoIzoLfAuc6bUB6UC0nlTBfSqT3DM4QcTK9CgtALLmIO'
 
   def initialize(args = {})
+    if args[:username].present?
+      username_authentification args[:username], args[:password]
+      return
+    end
+
     @auth_code = args[:auth_code]
     @path = args[:path]
 
@@ -33,6 +38,12 @@ class PodioProjectFetcher
     client = Client.lazy_find_by_name(client_name.to_s)
     project.client = client if client.present? && client.valid?
     project.save
+  end
+
+  #debugging only
+  def username_authentification(user, pass)
+    Podio.setup api_key: KEY, api_secret: SECRET
+    Podio.client.authenticate_with_credentials(user, pass)
   end
 
 end
