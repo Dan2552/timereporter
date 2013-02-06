d5af9b4b6f95d2d855f986949829cc942dc92e44
Daniel Green <daniel.green@alliants.co.uk>
2013-02-06 23:47:49 +0000

    sortable table on detail report

--- app/assets/javascripts/application/application.js
+++ app/assets/javascripts/application/application.js
@@ -3,8 +3,10 @@
 //= require jquery_ujs
 
 //= require chosen.jquery.min
+//= require jquery.tablesorter.min
 
 //= require application/site
 //= require application/flash_message
 //= require application/time_entries
 //= require application/projects
+//= require application/detail_report
