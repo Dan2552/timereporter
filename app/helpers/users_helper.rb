7774b14bb588487f4cecfcdcf05b3660a38b98ea
Bradley Jennings <bradley.jennings@alliants.co.uk>
2013-09-22 23:29:35 +0100

    update helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
-  def avatar
-    content_tag :div, image_tag(current_user.gravatar_url), class:'avatar'
+  def avatar_for(email)
+    content_tag :div, image_tag(email), class:'avatar'
   end
 
 end
