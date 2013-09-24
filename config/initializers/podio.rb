133f2da85e2f2fca2127643d1278cf087fa0bd1a
Daniel Green <dan2552@gmail.com>
2013-09-24 11:27:31 +0100

    Don't crash if podio.yml is missing or incomplete

--- config/initializers/podio.rb
+++ config/initializers/podio.rb
@@ -1,9 +1,13 @@
-config_file = YAML.load(File.read(File.expand_path('../../podio.yml', __FILE__)))
-credentials = config_file[Rails.env].symbolize_keys
+begin
+  config_file = YAML.load(File.read(File.expand_path('../../podio.yml', __FILE__)))
+  credentials = config_file[Rails.env].symbolize_keys
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :podio, credentials[:key], credentials[:secret]
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :podio, credentials[:key], credentials[:secret]
+  end
+
+  PodioProjectFetcher.auth_key = credentials[:key]
+  PodioProjectFetcher.auth_secret = credentials[:secret]
+rescue
+  puts "warning: podio.yml not loaded"
 end
-
-PodioProjectFetcher.auth_key = credentials[:key]
-PodioProjectFetcher.auth_secret = credentials[:secret]
